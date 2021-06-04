69401fc1b03c0ac0fff77526a10ccdb4a4a7da77
jayminPark <spjm2k@hotmail.com>
2021-06-04 16:50:49 +0900

    국선3년, 국선10년, 달러원선물 데이터 업데이트 코드 및 xlsx 업데이트.
    - 사용법 :
    1) 엑셀 업데이트 2 x 3 = 6개 하기(향후 3개로 줄일 예정)
    2) 파이썬 수행 10sec, tick부터 하고, convert 로직 수행.

--- dev/dataInsertion/1_everyday_tick_insert.py
+++ dev/dataInsertion/1_everyday_tick_insert.py
@@ -14,11 +14,8 @@ cursor = test_db.cursor(pymysql.cursors.DictCursor)
 """
 엑셀 데이터를 DB에 삽입해 주는 함
 """
-def insertKtbData(cursor):
-    df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\국선3년\ktbtickdata수집.xlsx', sheet_name=[1])
-    # df1 = df[1]
-    # df2 = df[2]
-    # df = df1.append(df2)
+def insertData(cursor, df_excel, table):
+
     df = df_excel[1]
     print(df)
     
@@ -26,62 +23,22 @@ def insertKtbData(cursor):
         code = str(df.iloc[i,0])
         date = str(df.iloc[i,1])[0:10]
         time = str(df.iloc[i,2])
-        # _open = str(df_excel.iloc[i,3])
-        # _hi = str(df_excel.iloc[i,4])
-        # _lo = str(df_excel.iloc[i,5])
-        _close = str(df.iloc[i,3])
+        close = str(df.iloc[i,3])
         vol = str(df.iloc[i,4])
         
-        sql = "insert into `ktbftick` (code, date, time, close, vol) values ('" + code+ "','" + date + "','" + time + "','" + _close + "','"+ vol +"'" + ');'
+        sql = "insert into " +table +" (code, date, time, close, volume) values ('" + code+ "','" + date + "','" + time + "','" + close + "','"+ vol +"'" + ');'
         cursor.execute(sql)
+    test_db.commit()
 
-def insertLktbData(cursor):
-    df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\국선10년\lktbtickdata수집.xlsx', sheet_name=[1])
-    # df1 = df[1]
-    # df2 = df[2]
-    # df = df1.append(df2)
-    df = df_excel[1]
-    print(df)
-    
-    for i in tqdm(range(len(df.index))) :
-        code = str(df.iloc[i,0])
-        date = str(df.iloc[i,1])[0:10]
-        time = str(df.iloc[i,2])
-        # _open = str(df_excel.iloc[i,3])
-        # _hi = str(df_excel.iloc[i,4])
-        # _lo = str(df_excel.iloc[i,5])
-        _close = str(df.iloc[i,3])
-        vol = str(df.iloc[i,4])
-        
-        sql = "insert into `lktbftick` (code, date, time, close, vol) values ('" + code+ "','" + date + "','" + time + "','" + _close + "','"+ vol +"'" + ');'
-        cursor.execute(sql)
 
-def insertUsdKrwData(cursor):
-    df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\원달러선물\달러원선물rawdata수집.xlsx', sheet_name=[1])
-    # df1 = df[1]
-    # df2 = df[2]
-    # df = df1.append(df2)
-    df = df_excel[1]
-    print(df)
-    
-    for i in tqdm(range(len(df.index))) :
-        code = str(df.iloc[i,0])
-        date = str(df.iloc[i,1])[0:10]
-        time = str(df.iloc[i,2])
-        # _open = str(df_excel.iloc[i,3])
-        # _hi = str(df_excel.iloc[i,4])
-        # _lo = str(df_excel.iloc[i,5])
-        _close = str(df.iloc[i,3])
-        vol = str(df.iloc[i,4])
-        
-        sql = "insert into `usdkrwtick` (code, date, time, close, vol) values ('" + code+ "','" + date + "','" + time + "','" + _close + "','"+ vol +"'" + ');'
-        cursor.execute(sql)
-    
+"""국10년선물 틱데이터"""
+df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\국선10년\lktbtickdata수집.xlsx', sheet_name=[1])
+insertData(cursor, df_excel, 'lktbftick')
 
-insertLktbData(cursor)
-test_db.commit()    
-insertKtbData(cursor)
-test_db.commit()
-insertUsdKrwData(cursor)
-test_db.commit()
+"""국3년선물 틱데이터"""
+df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\국선3년\ktbtickdata수집.xlsx', sheet_name=[1])
+insertData(cursor, df_excel, 'ktbftick')
 
+"""달러원선물 틱데이터"""
+df_excel = pd.read_excel('D:\dev\kbsecurities\dev\dataInsertion\달러원선물\달러원선물tickdata수집.xlsx', sheet_name=[1])
+insertData(cursor, df_excel, 'usdkrwtick')
---
+++ dev/dataInsertion/달러원선물/usdkrw1vo_insert.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from tqdm import tqdm
+import pymysql
+
+test_db = pymysql.connect(user='admin',
+                          passwd='se21121',
+                          # host = '211.232.156.57',
+                          host='127.0.0.1',
+                          db='testschema',
+                          charset='utf8')
+
+cursor = test_db.cursor(pymysql.cursors.DictCursor)
+
+
+"""
+엑셀 데이터를 DB에 삽입해 주는 함
+"""
+def insertExcelData(cursor, df):
+
+    for i in range(len(df.index)) :
+        code = str(df.iloc[i,0])
+        date = str(df.iloc[i,1])
+        time = str(df.iloc[i,2])[7:]
+        price = str(df.iloc[i,3])
+        sql = "insert into `usdkrw1vol` (code, date, time, price) values ('"  + code + "','" + date + "','" + time + "','" + price +"'" + ');'
+        cursor.execute(sql)
+        
+
+def setDfData(date_start, date_end) :
+    sql = "SELECT * FROM `usdkrwtick` where date >= '"+ str(date_start)[:10] + "' and date <= '" + str(date_end)[:10] + "';"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return pd.DataFrame(result)
+
+df_sample = setDfData('2017-12-20','2018-03-31')
+print(df_sample)
+print(len(df_sample.index))
+
+li = sorted(list(set(df_sample['date'])))
+
+
+for dt in li :
+    df_all = pd.DataFrame(columns=['code', 'date', 'time', 'price'])
+    df_today = df_sample[df_sample['date'] == dt]
+    i = 0
+    total_row = len(df_today.index)
+    
+    for row in tqdm(range(total_row)):
+        code = df_today.iloc[row]['code']
+        t = df_today.iloc[row]['time']
+        p = df_today.iloc[row]['close']
+        for v in range(df_today.iloc[row]['vol']):
+            i += 1
+    #             print(i)
+            df_all.at[i,'code'] = code
+            df_all.at[i,'date'] = dt
+            df_all.at[i,'time'] = t
+            df_all.at[i,'price'] = p
+
+    insertExcelData(cursor,df_all)
+    
+test_db.commit()
